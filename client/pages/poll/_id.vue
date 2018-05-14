31048987e853796d5efa7b0e8fe52052a464fd99
kaiogita <kaiogita@gmail.com>
2018-05-15 01:37:53 +0900

    add detail page

---
+++ client/pages/poll/_id.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        detail
+        id: {{detailPoll.id}}
+        title: {{detailPoll.title}}
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex'
+    export default {
+        name: "_id",
+        validate ({ params }) {
+            // 数値でなければならない
+            return /^\d+$/.test(params.id)
+        },
+        head () {
+            return {
+                title: this.detailPoll.title,
+                meta: [
+                    { hid: 'description', name: 'description', content: 'My custom description' }
+                ]
+            }
+        },
+        computed:mapState([
+            "detailPoll"
+        ]),
+        async fetch({ store, params }){
+            await store.dispatch("GET_DETAIL_POLL",{id: params.id})
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
