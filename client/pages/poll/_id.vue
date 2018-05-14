505d5226d4b19fd384336c24f274e26f64a136b8
kaiogita <kaiogita@gmail.com>
2018-05-15 02:13:49 +0900

    vote button追加

--- client/pages/poll/_id.vue
+++ client/pages/poll/_id.vue
@@ -16,14 +16,14 @@
         <section class="section">
             <div class="columns is-mobile is-multiline is-centered">
                 <div v-for="(value, key, index) in detailPoll.qs" :key="index" class="column is-3-desktop is-6-mobile">
-                    <div>
-                        <div class="box">
-                            <p>detail</p>
-                            <p>id: {{value.id}}</p>
-                            <p>title: {{value.ask}}</p>
-                            <p>vote: {{value.vote}}</p>
+                    <a @click="vote">
+                        <div class="box half-color-button">
+                                <p>detail</p>
+                                <p>id: {{value.id}}</p>
+                                <p>title: {{value.ask}}</p>
+                                <p>vote: {{value.vote}}</p>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </section>
@@ -49,12 +49,21 @@
         computed:mapState([
             "detailPoll"
         ]),
-        async fetch({ store, params }){
-            await store.dispatch("GET_DETAIL_POLL",{id: params.id})
+        fetch({ store, params }){
+            store.dispatch("GET_DETAIL_POLL",{id: params.id})
+        },
+        methods:{
+            vote(){
+                console.log("aaaaaaaaaaavote")
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .half-color-button{
+        background-color: #4098d3;
+        color: #fff;
+        background-image: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 50%, rgba(255,255,255,0.5) 51%);
+    }
 </style>
